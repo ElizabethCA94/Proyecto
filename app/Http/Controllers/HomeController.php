e7b0ed8d33be53be6245ff22f51041849740a4b4
Maria Camila <mmartinezbe@unal.edu.co>
2022-02-07 00:39:38 -0500

    Se agrega API que revisa el valor del dolar y euro para comparar el precio de los productos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Http;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $respuesta=Http::get('https://mindicador.cl/api');
+        $dolar = $respuesta->json();
+        return view('home', compact('dolar'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@ this is the request
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Tabla para calcular el precio de los productos en otras monedas') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -16,7 +16,30 @@ this is the request
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <table class="table text-center">
+  <thead>
+    <tr>
+        <td colspan="3" style="background-color: #3569DF; color: #ffffff"><strong>Valor del dolar y euro en Colombia</strong></td>
+    </tr>
+    
+    <tr>
+      <th>FECHA</th>
+      <th>DOLAR</thscope=>
+      <th>EURO</thscope=>
+     
+    </tr>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>{{ $dolar ['fecha']}}</th>
+      <td>{{ $dolar ['dolar']['valor']}}</td>
+      <td>{{ $dolar ['euro']['valor']}}</td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
                 </div>
             </div>
         </div>
