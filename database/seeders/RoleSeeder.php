e79ac2ba1df6aabb66a827d1c45958b115e3f597
ElizabethCA94 <ecarreno@unal.edu.co>
2022-02-07 01:52:32 -0500

    Se agrega seeder de rol  con los permisos

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -19,6 +19,27 @@ class RoleSeeder extends Seeder
         $role1 = Role::create(['name' => 'Admin']);
         $role2 = Role::create(['name' => 'Visitante']);
 
-        Permission::create(['name' => 'cli']);
+        Permission::create(['name' => 'admin.home']);
+
+        Permission::create(['name' => 'admin.categorias.index']);
+        Permission::create(['name' => 'admin.categorias.create']);
+        Permission::create(['name' => 'admin.categorias.edit']);
+        Permission::create(['name' => 'admin.categorias.destroy']);
+
+        Permission::create(['name' => 'admin.clientes.index']);
+        Permission::create(['name' => 'admin.clientes.create']);
+        Permission::create(['name' => 'admin.clientes.edit']);
+        Permission::create(['name' => 'admin.clientes.destroy']);
+
+        Permission::create(['name' => 'admin.ventas.index']);
+        Permission::create(['name' => 'admin.ventas.create']);
+        Permission::create(['name' => 'admin.ventas.edit']);
+        Permission::create(['name' => 'admin.ventas.destroy']);
+
+        Permission::create(['name' => 'admin.productos.index']);
+        Permission::create(['name' => 'admin.productos.create']);
+        Permission::create(['name' => 'admin.productos.edit']);
+        Permission::create(['name' => 'admin.productos.destroy']);
+
     }
 }
